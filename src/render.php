dc2898422553f1c7d0c7a41e6ec1e75f27543f28
M. L. Giannotta <sunyatasattva@gmail.com>
2024-10-03 14:37:23 +0800

    Render products on the front-end
    We need to replicate the HTML structure we have in the editor inside
    the `render.php` file. We can use WordPress PHP funcions to easily
    get our data on the server.

--- src/render.php
+++ src/render.php
@@ -3,7 +3,29 @@
  * @see https://github.com/WordPress/gutenberg/blob/trunk/docs/reference-guides/block-api/block-metadata.md#render
  */
 
+$currency_symbol = get_woocommerce_currency_symbol( get_woocommerce_currency() );
+$products = wc_get_products( array() );
+
 $wrapper_attributes = get_block_wrapper_attributes();
 ?>
 
-<div <?= $wrapper_attributes; ?>>Hello Tulum!</div>
+<div <?= $wrapper_attributes; ?>>
+	<ul class="products">
+		<?php foreach ($products as $product):
+			$data = $product->get_data();
+		?>
+			<li class="product">
+				<article>
+					<img
+						src="<?= wp_get_attachment_url( $data['image_id'] ); ?>"
+						class="product__image"
+					>
+					<h1 class="product__name"><?= $data['name']; ?></h1>
+					<div class="product__details">
+						<span class="product__price"><?= $currency_symbol . $data['price']; ?></span>
+					</div>
+				</article>
+			</li>
+		<?php endforeach; ?>
+	</ul>
+</div>
