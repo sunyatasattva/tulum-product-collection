7a737b82928ab1b7a3d4686164e04c61aed92936
M. L. Giannotta <sunyatasattva@gmail.com>
2024-10-03 17:04:09 +0800

    Add client-side navigation via the Interactivity API

--- src/render.php
+++ src/render.php
@@ -18,36 +18,44 @@ $max_num_pages = $response->max_num_pages;
 $wrapper_attributes = get_block_wrapper_attributes();
 ?>
 
-<div <?= $wrapper_attributes; ?>>
-	<ul class="products">
-		<?php foreach ($products as $product):
-			$data = $product->get_data();
-		?>
-			<li class="product">
-				<article>
-					<img
-						src="<?= wp_get_attachment_url( $data['image_id'] ); ?>"
-						class="product__image"
-					>
-					<h1 class="product__name"><?= $data['name']; ?></h1>
-					<div class="product__details">
-						<span class="product__price"><?= $currency_symbol . $data['price']; ?></span>
-					</div>
-				</article>
-			</li>
-		<?php endforeach; ?>
-	</ul>
-	<nav class="pagination">
-		<ul class="pagination__pages">
-			<?php for ( $i = 1; $i <= $max_num_pages; $i++ ): ?>
-				<li class="pagination__page">
-					<?php if ( $i === $page_number ): ?>
-						<span class="pagination__page--is-active"><?= $i; ?></span>
-					<?php else: ?>
-						<a class="pagination__page--link" href="?page_num=<?= $i; ?>"><?= $i; ?></a>
-					<?php endif; ?>
+<div <?= $wrapper_attributes; ?> data-wp-interactive="tulum-product-collection">
+	<div data-wp-router-region="tulum-product-collection">
+		<ul class="products">
+			<?php foreach ($products as $product):
+				$data = $product->get_data();
+			?>
+				<li class="product">
+					<article>
+						<img
+							src="<?= wp_get_attachment_url( $data['image_id'] ); ?>"
+							class="product__image"
+						>
+						<h1 class="product__name"><?= $data['name']; ?></h1>
+						<div class="product__details">
+							<span class="product__price"><?= $currency_symbol . $data['price']; ?></span>
+						</div>
+					</article>
 				</li>
-			<?php endfor; ?>
+			<?php endforeach; ?>
 		</ul>
-	</nav>
+		<nav class="pagination">
+			<ul class="pagination__pages">
+				<?php for ( $i = 1; $i <= $max_num_pages; $i++ ): ?>
+					<li class="pagination__page">
+						<?php if ( $i === $page_number ): ?>
+							<span class="pagination__page--is-active"><?= $i; ?></span>
+						<?php else: ?>
+							<a
+								class="pagination__page--link"
+								href="?page_num=<?= $i; ?>"
+								data-wp-on--click="actions.navigate"
+							>
+								<?= $i; ?>
+							</a>
+						<?php endif; ?>
+					</li>
+				<?php endfor; ?>
+			</ul>
+		</nav>
+	</div>
 </div>
--- src/view.js
+++ src/view.js
@@ -1,3 +1,18 @@
 /**
  * The JavaScript code loaded in the front-end.
  */
+
+import { store, getElement } from "@wordpress/interactivity";
+import { actions } from "@wordpress/interactivity-router";
+
+store( "tulum-product-collection", {
+	actions: {
+		*navigate( event ) {
+			event.preventDefault();
+
+			const { ref } = getElement();
+			console.log(ref.href);
+			yield actions.navigate( ref.href );
+		},
+	},
+} );
